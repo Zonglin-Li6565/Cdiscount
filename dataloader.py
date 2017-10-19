3a416fa15b17be4674480b9bf88408dfca7b3045
Zonglin-Li6565 <development.my6565@gmail.com>
2017-10-19 18:40:15 -0500

    dummy preprocessor

--- dataloader.py
+++ dataloader.py
@@ -5,14 +5,15 @@ This module contains data preprocessor and pipelined data feeder
 from __future__ import print_function
 
 import io
-import bson
 import numpy as np
 import pandas as pd
 import tensorflow as tf
 from skimage.data import imread
 import multiprocessing as mp
+import os.path
 
 IMAGE_SHAPE = (180, 180, 3)
+NCORE = 8
 
 _img_dir = None
 _sess = None
@@ -33,8 +34,31 @@ _data_queue = tf.FIFOQueue(
         ()                                  # single integer
     ))
 
+_threads = []
+
 def _preprocess(bson_path, image_dir):
-    pass
+
+    def worker(queue, iolock):
+        while True:
+            d = q.get()
+            if d is None:
+                break
+            product_id = d["_id"]
+            category_id = d["category_id"]
+            product = [category_id]
+            for e, pic in enumerate(d["imgs"]):
+                picture = imread(io.BytesIO(pic["picture"]))
+                product.append(picture)
+            path = os.path.join(image_dir, str(product_id) + ".p")
+            with open(path, "wb") as f:
+                pickle.dump(product, f)
+
+    q = mp.Queue(maxsize=NCORE)
+    iolock = mp.Lock()
+    pool = mp.Pool(NCORE, initializer=worker, initargs=(q, iolock))
+    data = bson.decode_file_iter(open(bson_path, "rb"))
+    for c, d in enumerate(data):
+        q.put(d)
 
 def check_preprocess(bson_path, image_dir):
     """
@@ -62,4 +86,10 @@ def end():
 
     Return: None
     """
-    pass
+    if _sess is None:
+        return
+    _coord.request_stop()
+    dq_stop = _data_queue.close(cancel_pending_enqueues=True)
+    _sess.run(dq_stop)
+    _coord.join(_threads)
+
